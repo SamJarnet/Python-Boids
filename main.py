2043e903f33562753e8aeed2c746525ec4dea4e7
SamuelJarnet <samueljarnet2006@gmail.com>
2026-06-19 12:35:57 +0100

    added sliders and alignment]

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 from matplotlib.patches import Circle, Polygon
 import random
+from matplotlib.widgets import Button, Slider
+
 
 class Boids:
     def __init__(self):
@@ -19,15 +21,40 @@ class Boids:
         
         self.group_circles = []
         self.boids = []
-        for i in range(0, 10):
+        for i in range(0, 20):
             vertices = np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
             triangle = Polygon(vertices.tolist(), fc='black', zorder=3)
             self.boids.append({"shape": triangle,"pos": np.array([self.x_width//2 + i, self.y_width//2]), "vel": np.array([random.randint(1, 3)/3, random.randint(1, 3)/3])})
 
         self.dt = 0.1
         self.group_radius = 1
-        self.cohesion_strength = 0.1
-        self.seperation_strength = 0.025
+        self.cohesion_strength = 0.134
+        self.seperation_strength = 0.045
+        self.alignment_strength = 0.038
+
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        cohesion = plt.axes([0.08, 0.08, 0.2, 0.03]) # type: ignore
+        self.cohesion_slider = Slider(cohesion, 'Cohesion', 0, 0.2, valinit=self.cohesion_strength, valfmt='%1.3f')
+        self.cohesion_slider.on_changed(self.update_cohesion)
+
+        seperation = plt.axes([0.41, 0.08, 0.2, 0.03]) # type: ignore
+        self.seperation_slider = Slider(seperation, 'Separation', 0, 0.5, valinit=self.seperation_strength, valfmt='%1.3f')
+        self.seperation_slider.on_changed(self.update_seperation)
+
+        alignment = plt.axes([0.75, 0.08, 0.2, 0.03]) # type: ignore
+        self.alignment_slider = Slider(alignment, 'Alignment', 0, 0.2, valinit=self.alignment_strength, valfmt='%1.3f')
+        self.alignment_slider.on_changed(self.update_alignment)
+
+    def update_cohesion(self, n):
+        self.cohesion_strength = n
+
+    def update_seperation(self, n):
+        self.seperation_strength = n
+    
+    def update_alignment(self, n):
+        self.alignment_strength = n
 
     def init(self):
         for i in range(0, len(self.boids)):
@@ -91,7 +118,7 @@ class Boids:
                             search.append(j)           
         return groups
 
-    def find_average_pos(self, groups):
+    def find_average(self, groups, target):
         group_positions = []
         
         for group in groups: 
@@ -104,8 +131,8 @@ class Boids:
 
             if len(group) > 1:
                 for boid in group:
-                    sum_x += self.boids[boid]["pos"][0]
-                    sum_y += self.boids[boid]["pos"][1]
+                    sum_x += self.boids[boid][target][0]
+                    sum_y += self.boids[boid][target][1]
                     
                 avg_x = sum_x / group_count
                 avg_y = sum_y / group_count
@@ -113,14 +140,14 @@ class Boids:
 
         return group_positions
     
+    
 
     def cohesion(self, groups, centers, boid):
-
-            
         for i, group in enumerate(groups):
             if boid in group:
                 if centers[i] is None:
                     return np.array([0.0, 0.0])
+                
                 difference = np.array(centers[i] - self.boids[boid]["pos"]) # get vector in direction of average point
                 return (difference * self.cohesion_strength)
             
@@ -130,23 +157,45 @@ class Boids:
         for group in groups:
             if boid in group and len(group) > 1:
                 vec = np.array([0.0, 0.0])
+
                 for boid_id in group:
                     if boid_id != boid:
-                        displacement = self.boids[boid]["pos"] - self.boids[boid_id]["pos"]
-                        mod_r = np.linalg.norm(displacement)
+                        displacement = self.boids[boid]["pos"] - self.boids[boid_id]["pos"]  
+                        mod_r = np.linalg.norm(displacement)  # direction to each other boid
+
                         if 0 < mod_r < 1:
-                            vec += displacement / mod_r
+                            vec += displacement / mod_r  
+
                 return vec * self.seperation_strength
         return np.array([0.0, 0.0])
+    
+
+    def allignment(self, groups, avg_vels, boid):
+        for i, group in enumerate(groups):
+            if boid in group:
+                if avg_vels[i] is None:
+                    return np.array([0.0, 0.0])
+                
+                difference = np.array(avg_vels[i] - self.boids[boid]["vel"]) # get vector of average vels
+                return (difference * self.alignment_strength)
+            
+        return np.array([0.0, 0.0])
+    
+    def cap_speed(self):
+        pass
 
     def step(self):
         self.check_boundaries()
         groups = self.find_groups()
-        group_avg_pos = self.find_average_pos(groups)
+        group_avg_pos = self.find_average(groups, "pos")
+        group_avg_vel = self.find_average(groups, "vel")
+
         for i in range(0, len(self.boids)):
             cohesion_force = self.cohesion(groups, group_avg_pos, i)
             seperation_force = self.seperation(groups, i)
-            self.boids[i]["vel"] += cohesion_force + seperation_force
+            alignment_force = self.allignment(groups, group_avg_vel, i)
+
+            self.boids[i]["vel"] += cohesion_force + seperation_force + alignment_force
             self.boids[i]["pos"] += self.boids[i]["vel"] * self.dt
 
         return group_avg_pos
@@ -182,7 +231,6 @@ class Boids:
             circle = Circle((pos[0], pos[1]), 0.1, fc='blue', alpha=0.6, zorder=2)
             self.ax.add_patch(circle)
             self.group_circles.append(circle)
-        
 
         return []
     
